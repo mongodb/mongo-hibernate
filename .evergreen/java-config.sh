7de787ad35331eb7c799d25030b09d60cc6be508
Nathan Xu <nathan.xu@mongodb.com>
2024-10-11 20:18:03 -0400

    add evergreen integration

---
+++ .evergreen/java-config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Java configurations for evergreen
+
+export JDK8="/opt/java/jdk8"
+export JDK11="/opt/java/jdk11"
+export JDK17="/opt/java/jdk17"
+export JDK21="/opt/java/jdk21"
+
+if [ -d "$JDK17" ]; then
+  export JAVA_HOME=$JDK17
+fi
+
+export JAVA_VERSION=${JAVA_VERSION:-17}
+
+echo "Java Configs:"
+echo "Java Home: ${JAVA_HOME}"
+echo "Java test version: ${JAVA_VERSION}"
---
+++ .evergreen/static-check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o xtrace   # Write all commands first to stderr
+set -o errexit  # Exit the script with error if any of the commands fail
+
+############################################
+#            Main Program                  #
+############################################
+source ./.evergreen/java-config.sh
+
+echo "mongo-hibernate: static checking ..."
+
+./gradlew -version
+./gradlew -PxmlReports.enabled=true --info -x test clean check
