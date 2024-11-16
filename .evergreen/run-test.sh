fb84a542ce3314c2f527354bf8daa9fd4466e9d3
Nathan Xu <nathan.xu@mongodb.com>
2024-11-15 20:22:20 -0500

    fix some defects found during manual testing

--- .evergreen/run-test.sh
+++ .evergreen/run-test.sh
@@ -9,7 +9,7 @@ set -o errexit  # Exit the script with error if any of the commands fail
 
 source java-config.sh
 
-echo "${PROJECT}: running tests ..."
+echo "mongo-hibernate: running tests ..."
 
 echo "MongoDB version: ${MONGODB_VERSION}; topology: ${TOPOLOGY}"
 
--- .evergreen/static-check.sh
+++ .evergreen/static-check.sh
@@ -8,7 +8,8 @@ set -o errexit  # Exit the script with error if any of the commands fail
 ############################################
 source java-config.sh
 
-echo "${PROJECT}: static checking ..."
+echo "mongo-hibernate: static checking ..."
 
 ./gradlew -version
-./gradlew -PxmlReports.enabled=true --info -x test clean check compileJava
+
+./gradlew --info -x test clean check compileJava
