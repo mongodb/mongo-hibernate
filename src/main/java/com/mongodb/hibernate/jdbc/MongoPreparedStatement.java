75db3d9c49a8e455b67f8a3799e41fa488b1c5b8
Nathan Xu <nathan.xu@mongodb.com>
2025-01-20 16:14:22 -0500

    move checkClosed to public methods instead of common private method

--- src/main/java/com/mongodb/hibernate/jdbc/MongoPreparedStatement.java
+++ src/main/java/com/mongodb/hibernate/jdbc/MongoPreparedStatement.java
@@ -98,66 +98,79 @@ final class MongoPreparedStatement extends MongoStatement implements PreparedSta
 
     @Override
     public void setBoolean(int parameterIndex, boolean x) throws SQLException {
+        checkClosed();
         setParameter(parameterIndex, BsonBoolean.valueOf(x));
     }
 
     @Override
     public void setByte(int parameterIndex, byte x) throws SQLException {
+        checkClosed();
         setInt(parameterIndex, x);
     }
 
     @Override
     public void setShort(int parameterIndex, short x) throws SQLException {
+        checkClosed();
         setInt(parameterIndex, x);
     }
 
     @Override
     public void setInt(int parameterIndex, int x) throws SQLException {
+        checkClosed();
         setParameter(parameterIndex, new BsonInt32(x));
     }
 
     @Override
     public void setLong(int parameterIndex, long x) throws SQLException {
+        checkClosed();
         setParameter(parameterIndex, new BsonInt64(x));
     }
 
     @Override
     public void setFloat(int parameterIndex, float x) throws SQLException {
+        checkClosed();
         setDouble(parameterIndex, x);
     }
 
     @Override
     public void setDouble(int parameterIndex, double x) throws SQLException {
+        checkClosed();
         setParameter(parameterIndex, new BsonDouble(x));
     }
 
     @Override
     public void setBigDecimal(int parameterIndex, @Nullable BigDecimal x) throws SQLException {
+        checkClosed();
         setParameter(parameterIndex, x == null ? BsonNull.VALUE : new BsonDecimal128(new Decimal128(x)));
     }
 
     @Override
     public void setString(int parameterIndex, @Nullable String x) throws SQLException {
+        checkClosed();
         setParameter(parameterIndex, x == null ? BsonNull.VALUE : new BsonString(x));
     }
 
     @Override
     public void setBytes(int parameterIndex, byte @Nullable [] x) throws SQLException {
+        checkClosed();
         setParameter(parameterIndex, x == null ? BsonNull.VALUE : new BsonBinary(x));
     }
 
     @Override
     public void setDate(int parameterIndex, @Nullable Date x) throws SQLException {
+        checkClosed();
         setBsonDateTimeParameter(parameterIndex, x);
     }
 
     @Override
     public void setTime(int parameterIndex, @Nullable Time x) throws SQLException {
+        checkClosed();
         setBsonDateTimeParameter(parameterIndex, x);
     }
 
     @Override
     public void setTimestamp(int parameterIndex, @Nullable Timestamp x) throws SQLException {
+        checkClosed();
         setBsonDateTimeParameter(parameterIndex, x);
     }
 
@@ -240,7 +253,6 @@ final class MongoPreparedStatement extends MongoStatement implements PreparedSta
     }
 
     private void setParameter(int parameterIndex, BsonValue parameterValue) throws SQLException {
-        checkClosed();
         if (parameterIndex < 1 || parameterIndex > parameterValueSetters.size()) {
             throw new SQLException(format(
                     "Parameter index invalid: %d; should be within [1, %d]",
