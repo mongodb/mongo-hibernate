8775eb407cab80f6dc5813bb768ecda47435cc7c
Nathan Xu <nathan.qingyang.xu@gmail.com>
2025-02-06 18:48:08 -0500

    Update src/main/java/com/mongodb/hibernate/internal/mongoast/AstPlaceholder.java
    Co-authored-by: Valentin Kovalenko <valentin.male.kovalenko@gmail.com>

--- src/main/java/com/mongodb/hibernate/internal/mongoast/AstPlaceholder.java
+++ src/main/java/com/mongodb/hibernate/internal/mongoast/AstPlaceholder.java
@@ -20,7 +20,7 @@ import org.bson.BsonWriter;
 
 /**
  * Represents an MQL parameter placeholder, whose values will be provided to {@link java.sql.PreparedStatement}'s
- * various setter methods together with the their position indexes.
+ * various setter methods together with their position indexes.
  *
  * <p>Note that MQL has no SQL parameter placeholder (JDBC uses {@code ?} as placeholder marker) counterpart; currently
  * {@code {"$undefined": true}} is chosen
