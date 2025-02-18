639b325ddf4fd517e313bd747d76462756455d0b
Valentin Kovalenko <valentin.kovalenko@mongodb.com>
2025-02-18 14:03:03 -0700

    Call `flush` and `close` where they are supposed to (#45)
    This reinstates the change proposed and accepted in https://github.com/mongodb/mongo-hibernate/pull/27#discussion_r1944020975.

--- src/main/java/com/mongodb/hibernate/internal/translate/AbstractMqlTranslator.java
+++ src/main/java/com/mongodb/hibernate/internal/translate/AbstractMqlTranslator.java
@@ -25,6 +25,7 @@ import com.mongodb.hibernate.internal.translate.mongoast.AstElement;
 import com.mongodb.hibernate.internal.translate.mongoast.AstNode;
 import com.mongodb.hibernate.internal.translate.mongoast.AstPlaceholder;
 import com.mongodb.hibernate.internal.translate.mongoast.command.AstInsertCommand;
+import java.io.IOException;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
@@ -166,9 +167,14 @@ abstract class AbstractMqlTranslator<T extends JdbcOperation> implements SqlAstT
     }
 
     static String renderMongoAstNode(AstNode rootAstNode) {
-        var writer = new StringWriter();
-        rootAstNode.render(new JsonWriter(writer, JSON_WRITER_SETTINGS));
-        return writer.toString();
+        try (var stringWriter = new StringWriter();
+                var jsonWriter = new JsonWriter(stringWriter, JSON_WRITER_SETTINGS)) {
+            rootAstNode.render(jsonWriter);
+            jsonWriter.flush();
+            return stringWriter.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     <R extends AstNode> R acceptAndYield(Statement statement, AstVisitorValueDescriptor<R> resultDescriptor) {
