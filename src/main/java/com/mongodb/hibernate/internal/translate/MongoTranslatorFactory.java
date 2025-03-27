11a654584794a5dc4f71654d4daf838b003e3c9b
Valentin Kovalenko <valentin.kovalenko@mongodb.com>
2025-03-26 22:35:28 -0600

    Document that `MongoTranslatorFactory` is thread-safe.

--- src/main/java/com/mongodb/hibernate/internal/translate/MongoTranslatorFactory.java
+++ src/main/java/com/mongodb/hibernate/internal/translate/MongoTranslatorFactory.java
@@ -26,6 +26,9 @@ import org.hibernate.sql.exec.spi.JdbcOperationQuerySelect;
 import org.hibernate.sql.model.ast.TableMutation;
 import org.hibernate.sql.model.jdbc.JdbcMutationOperation;
 
+/**
+ * Tread-safe.
+ */
 public final class MongoTranslatorFactory implements SqlAstTranslatorFactory {
     public static MongoTranslatorFactory INSTANCE = new MongoTranslatorFactory();
 
