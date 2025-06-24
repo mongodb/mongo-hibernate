2d35bc6b84c4eb010a265539783f3d732632dc9c
Valentin Kovalenko <valentin.kovalenko@mongodb.com>
2025-06-24 15:41:55 -0600

    Make use of the project name consistent with the POM project `name` (#108)

--- src/main/java/com/mongodb/hibernate/internal/cfg/MongoConfiguration.java
+++ src/main/java/com/mongodb/hibernate/internal/cfg/MongoConfiguration.java
@@ -21,7 +21,7 @@ import com.mongodb.hibernate.cfg.MongoConfigurator;
 import java.util.function.Consumer;
 
 /**
- * The configuration of the MongoDB extension of Hibernate ORM.
+ * The configuration of the MongoDB Hibernate Extension.
  *
  * @param mongoClientSettings {@link MongoConfigurator#applyToMongoClientSettings(Consumer)}.
  * @param databaseName {@link MongoConfigurator#databaseName(String)}.
