d7aefc5b283960f25a0b6d5235f141c573fb2f79
Nathan Xu <nathan.qingyang.xu@gmail.com>
2025-08-14 13:18:46 -0400

    use Hibernate's short-name strategy mechanism to avoid exposing implementation details

--- src/main/java/com/mongodb/hibernate/internal/MongoConstants.java
+++ src/main/java/com/mongodb/hibernate/internal/MongoConstants.java
@@ -29,4 +29,7 @@ public final class MongoConstants {
     public static final String MONGO_DBMS_NAME = "MongoDB";
     public static final String MONGO_JDBC_DRIVER_NAME = "MongoDB Java Driver JDBC Adapter";
     public static final String ID_FIELD_NAME = "_id";
+
+    public static final String MONGO_DIALECT_SHORT_NAME = "mongodb";
+    public static final String MONGO_CONNECTION_PROVIDER_SHORT_NAME = "mongodb-connection-provider";
 }
