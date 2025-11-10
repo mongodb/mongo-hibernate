f9502940fc36e88bd883af0cac43bd4ed041cbe6
Valentin Kovalenko <valentin.kovalenko@mongodb.com>
2025-11-10 14:19:48 -0700

    Use `FeatureNotSupportedException` instead of `RuntimeException` where appropriate
    HIBERNATE-45

--- src/main/java/com/mongodb/hibernate/jdbc/MongoStatement.java
+++ src/main/java/com/mongodb/hibernate/jdbc/MongoStatement.java
@@ -150,7 +150,7 @@ class MongoStatement implements StatementAdapter {
         var exclude = (value.isBoolean() && !value.asBoolean().getValue())
                 || (value.isNumber() && value.asNumber().intValue() == 0);
         if (exclude && !key.equals(ID_FIELD_NAME)) {
-            throw new RuntimeException(format(
+            throw new FeatureNotSupportedException(format(
                     "Exclusions are not allowed in `$project` specifications, except for the [%s] field: [%s, %s]",
                     ID_FIELD_NAME, key, value));
         }
