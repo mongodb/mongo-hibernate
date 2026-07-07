874dc3501011e63bce39015e095774261ba0fc99
Jeff Yemin <jeff.yemin@mongodb.com>
2026-07-07 17:36:27 -0400

    HIBERNATE-73: test each forbidden BSON/UUID type as a basic attribute

--- src/integrationTest/java/com/mongodb/hibernate/type/UnsupportedTypesIntegrationTests.java
+++ src/integrationTest/java/com/mongodb/hibernate/type/UnsupportedTypesIntegrationTests.java
@@ -21,6 +21,19 @@ import static com.mongodb.hibernate.type.UnsupportedTypeAssertions.assertNotSupp
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import java.util.UUID;
+import org.bson.BsonDocument;
+import org.bson.BsonDocumentWrapper;
+import org.bson.Document;
+import org.bson.RawBsonDocument;
+import org.bson.types.BSONTimestamp;
+import org.bson.types.Binary;
+import org.bson.types.Code;
+import org.bson.types.CodeWScope;
+import org.bson.types.CodeWithScope;
+import org.bson.types.Decimal128;
+import org.bson.types.MaxKey;
+import org.bson.types.MinKey;
+import org.bson.types.Symbol;
 import org.junit.jupiter.api.Test;
 
 class UnsupportedTypesIntegrationTests {
@@ -30,11 +43,181 @@ class UnsupportedTypesIntegrationTests {
         assertNotSupported(UuidItem.class);
     }
 
+    @Test
+    void bsonTimestampBasicAttributeNotSupported() {
+        assertNotSupported(BsonTimestampItem.class);
+    }
+
+    @Test
+    void binaryBasicAttributeNotSupported() {
+        assertNotSupported(BinaryItem.class);
+    }
+
+    @Test
+    void codeBasicAttributeNotSupported() {
+        assertNotSupported(CodeItem.class);
+    }
+
+    @Test
+    void codeWithScopeBasicAttributeNotSupported() {
+        assertNotSupported(CodeWithScopeItem.class);
+    }
+
+    @Test
+    void codeWScopeBasicAttributeNotSupported() {
+        assertNotSupported(CodeWScopeItem.class);
+    }
+
+    @Test
+    void minKeyBasicAttributeNotSupported() {
+        assertNotSupported(MinKeyItem.class);
+    }
+
+    @Test
+    void maxKeyBasicAttributeNotSupported() {
+        assertNotSupported(MaxKeyItem.class);
+    }
+
+    @Test
+    void symbolBasicAttributeNotSupported() {
+        assertNotSupported(SymbolItem.class);
+    }
+
+    @Test
+    void decimal128BasicAttributeNotSupported() {
+        assertNotSupported(Decimal128Item.class);
+    }
+
+    @Test
+    void documentBasicAttributeNotSupported() {
+        assertNotSupported(DocumentItem.class);
+    }
+
+    @Test
+    void bsonDocumentBasicAttributeNotSupported() {
+        assertNotSupported(BsonDocumentItem.class);
+    }
+
+    @Test
+    void rawBsonDocumentBasicAttributeNotSupported() {
+        assertNotSupported(RawBsonDocumentItem.class);
+    }
+
+    @Test
+    void bsonDocumentWrapperBasicAttributeNotSupported() {
+        assertNotSupported(BsonDocumentWrapperItem.class);
+    }
+
     @Entity
     static class UuidItem {
         @Id
         int id;
 
         UUID v;
+    }
+
+    @Entity
+    static class BsonTimestampItem {
+        @Id
+        int id;
+
+        BSONTimestamp v;
+    }
+
+    @Entity
+    static class BinaryItem {
+        @Id
+        int id;
+
+        Binary v;
+    }
+
+    @Entity
+    static class CodeItem {
+        @Id
+        int id;
+
+        Code v;
+    }
+
+    @Entity
+    static class CodeWithScopeItem {
+        @Id
+        int id;
+
+        CodeWithScope v;
+    }
+
+    @Entity
+    static class CodeWScopeItem {
+        @Id
+        int id;
+
+        CodeWScope v;
+    }
+
+    @Entity
+    static class MinKeyItem {
+        @Id
+        int id;
+
+        MinKey v;
+    }
+
+    @Entity
+    static class MaxKeyItem {
+        @Id
+        int id;
+
+        MaxKey v;
+    }
+
+    @Entity
+    static class SymbolItem {
+        @Id
+        int id;
+
+        Symbol v;
+    }
+
+    @Entity
+    static class Decimal128Item {
+        @Id
+        int id;
+
+        Decimal128 v;
+    }
+
+    @Entity
+    static class DocumentItem {
+        @Id
+        int id;
+
+        Document v;
+    }
+
+    @Entity
+    static class BsonDocumentItem {
+        @Id
+        int id;
+
+        BsonDocument v;
+    }
+
+    @Entity
+    static class RawBsonDocumentItem {
+        @Id
+        int id;
+
+        RawBsonDocument v;
+    }
+
+    @Entity
+    static class BsonDocumentWrapperItem {
+        @Id
+        int id;
+
+        @SuppressWarnings("rawtypes")
+        BsonDocumentWrapper v;
     }
 }
